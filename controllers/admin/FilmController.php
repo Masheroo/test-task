5477c033a2ee193212370aee9b4f39809f5cc5b7
mashero <dima.vuynov@yandex.ru>
2023-07-17 16:43:46 +0300

    fix(FilmController): удалено использование метода setDefaultValue в actionCreate, т.к. в базе нету значений по умолчанию

--- controllers/admin/FilmController.php
+++ controllers/admin/FilmController.php
@@ -82,8 +82,6 @@ class FilmController extends Controller
                 }
 
             }
-        } else {
-            $film->loadDefaultValues();
         }
 
         return $this->render('create', [
